013f04b8b34fc53205e2d3454a66f7394cfb9454
Shreya Wadhwa <shreyawadhwa369@gmail.com>
2020-04-22 10:43:00 -0500

    product reducer

--- client/store/reducers/productReducer.js
+++ client/store/reducers/productReducer.js
@@ -4,7 +4,8 @@ import axios from 'axios'
  * INITIAL STATE
  */
 const initialState = {
-  productList: {},
+  allProducts: [],
+  singleProduct: {},
   isFetching: false
 }
 
@@ -12,6 +13,7 @@ const initialState = {
  * ACTION TYPES
  */
 const GET_PRODUCTS = 'GET_PRODUCTS'
+const GET_PRODUCT = 'GET_PRODUCT'
 const CREATE_PRODUCT = 'CREATE_PRODUCT'
 const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
 const CREATE_REVIEW = 'CREATE_REVIEW'
@@ -20,7 +22,18 @@ const CREATE_REVIEW = 'CREATE_REVIEW'
  * ACTION CREATORS
  */
 
-export const getProducts = () => {}
+export const getProducts = products => {
+  return {
+    type: GET_PRODUCTS,
+    products
+  }
+}
+export const getProduct = product => {
+  return {
+    type: GET_PRODUCT,
+    product
+  }
+}
 export const createProduct = () => {}
 export const updateProduct = () => {}
 export const createReview = () => {}
@@ -28,7 +41,27 @@ export const createReview = () => {}
 /**
  * THUNK CREATORS
  */
-export const fetchProducts = () => async dispatch => {}
+export function fetchProducts() {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get(`/api/`)
+      dispatch(getProducts(data))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+export function fetchProduct(productId) {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get(`/api/${productId}`)
+      dispatch(getProduct(data))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
 export const addProduct = () => async dispatch => {}
 export const editProduct = () => async dispatch => {}
 export const addReview = () => async dispatch => {}
@@ -38,6 +71,12 @@ export const addReview = () => async dispatch => {}
  */
 export default function productReducer(state = initialState, action) {
   switch (action.type) {
+    case GET_PRODUCTS:
+      return {...state, allProducts: state.allProducts.concat(action.products)}
+    case GET_PRODUCT: {
+      const newSingProduct = {...state, singleProduct: action.product}
+      return newSingProduct
+    }
     default:
       return state
   }

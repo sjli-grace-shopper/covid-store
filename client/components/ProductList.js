e59ece4c9e369732a9e76bbeb43470bd86ba82af
Shreya Wadhwa <shreyawadhwa369@gmail.com>
2020-04-22 11:35:52 -0500

    temp

--- client/components/ProductList.js
+++ client/components/ProductList.js
@@ -1,2 +1,31 @@
 // Products component
 // mapState - state.user, state.products.list.filter(by category), category(from URL?)
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+
+const AllProducts = props => {
+  return (
+    <div className="allProducts">
+      {props.products.map(product => {
+        return (
+          <div key={product.id} className="product">
+            <Link to={`/products/${product.id}`}>
+              <img className="product-img" src={product.imageUrl} />
+              <h3>{product.name}</h3>
+            </Link>
+            <p>{product.price} USD</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    products: state.products.productList
+  }
+}
+
+export default connect(mapStateToProps)(AllProducts)

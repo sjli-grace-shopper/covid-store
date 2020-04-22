83c10efce5b15263e286ef18edcd66ff645c7f2f
Lauryn Bassett <lauryn.bassett@gmail.com>
2020-04-22 14:17:17 -0500

    added cart routes, AddCartItemButton and SingleProduct components

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -2,25 +2,63 @@
 import React, {useState, useEffect} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {makeStyles} from '@material-ui/core/styles'
+import Breadcrumbs from '@material-ui/core/Breadcrumbs'
+import Typography from '@material-ui/core/Typography'
+import NavigateNextIcon from '@material-ui/icons/NavigateNext'
 
-import AddCartItem from 'module'
+import AddCartItemButton from './AddCartItemButton'
 import {addCartItem} from '../store/reducers/cartReducer'
+import history from '../history'
 
-export const SingleProduct = () => {}
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > * + *': {
+      marginTop: theme.spacing(2)
+    }
+  }
+}))
+
+const handleClick = evt => {
+  evt.preventDefault()
+  // history.push(`/categories/${}`)
+  // history.push('/products')
+}
+
+export const SingleProduct = props => {
+  const {product, user} = props
+  const classes = useStyles()
+
+  if (product.id)
+    return (
+      <div className={classes.root}>
+        <Breadcrumbs separator="›" aria-label="breadcrumb">
+          <Link color="inherit" href="/" onClick={handleClick}>
+            Material-UI
+          </Link>
+          <Link
+            color="inherit"
+            href="/getting-started/installation/"
+            onClick={handleClick}
+          >
+            Core
+          </Link>
+          <Typography color="textPrimary">Breadcrumb</Typography>
+        </Breadcrumbs>
+      </div>
+    )
+  else return null
+}
 
 const mapState = (state, ownProps) => {
-  const id = +ownProps.match.params.campusId
-  const getCampus = state.campuses.find(campus => campus.id === id)
-  const getStudents = state.students.filter(student => student.campusId === id)
+  const id = +ownProps.match.params.productId
+  const getProduct = state.products.productList.find(
+    product => product.id === id
+  )
   return {
-    campuses: state.campuses,
-    campus: getCampus,
-    students: getStudents
+    product: getProduct,
+    user: state.user
   }
 }
 
-const mapDispatch = (dispatch, ownProps) => ({
-  addCartItem: id => dispatch(addCartItem(id, ownProps))
-})
-
-export default connect(mapState, mapDispatch)(SingleProduct)
+export default connect(mapState)(SingleProduct)
--- client/store/reducers/cartReducer.js
+++ client/store/reducers/cartReducer.js
@@ -20,24 +20,106 @@ const CLEAR_CART = 'CLEAR_CART'
 /**
  * ACTION CREATORS
  */
-export const getCart = () => {}
-export const updateCart = () => {}
-export const deleteCartItem = () => {}
-export const clearCart = () => {}
+export const getCart = cart => ({
+  type: GET_CART,
+  cart
+})
+
+export const addToCart = item => ({
+  type: ADD_TO_CART,
+  item
+})
+
+export const updateCart = item => ({
+  type: UPDATE_CART,
+  item
+})
+
+export const deleteCartItem = id => ({
+  type: DELETE_CART_ITEM,
+  id
+})
+
+export const clearCart = () => ({
+  type: CLEAR_CART
+})
 
 /**
  * THUNK CREATORS
  */
-export const fetchCart = () => async dispatch => {}
-export const addCartItem = () => async dispatch => {}
-export const editCart = () => async dispatch => {}
-export const removeCartItem = () => async dispatch => {}
+export const fetchCart = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/cart')
+    dispatch(getCart(data))
+  } catch (err) {
+    console.error('Error fetching cart: ', err)
+  }
+}
+
+export const addCartItem = item => async dispatch => {
+  try {
+    const {data} = await axios.post('/api/cart', item)
+    dispatch(addToCart(data))
+  } catch (err) {
+    console.error('Error adding cart item: ', err)
+  }
+}
+
+export const editCart = item => async dispatch => {
+  try {
+    const {data} = await axios.put('/api/cart', item)
+    dispatch(updateCart(data))
+  } catch (err) {
+    console.error('Error updating cart: ', err)
+  }
+}
+
+export const removeCartItem = id => async dispatch => {
+  try {
+    await axios.delete(`/api/cart/${id}`)
+    dispatch(deleteCartItem(id))
+  } catch (err) {
+    console.error('Error removing cart item: ', err)
+  }
+}
 
 /**
  * REDUCER
  */
 export default function cartReducer(state = initialState, action) {
   switch (action.type) {
+    case GET_CART:
+      return {...state, isFetching: true, cart: action.cart}
+    case ADD_TO_CART:
+      return {
+        ...state,
+        isFetching: true,
+        cart: {...state.cart, products: [...state.cart.products, action.item]}
+      }
+    case UPDATE_CART:
+      return {
+        ...state,
+        isFetching: true,
+        cart: {
+          ...state.cart,
+          products: state.cart.products.map(product => {
+            return product.id === action.item.id ? action.item : product
+          })
+        }
+      }
+    case DELETE_CART_ITEM:
+      return {
+        ...state,
+        isFetching: true,
+        cart: {
+          ...state.cart,
+          products: state.cart.products.filter(
+            product => product.id !== action.id
+          )
+        }
+      }
+    case CLEAR_CART:
+      return {...state, isFetching: true, cart: {}}
     default:
       return state
   }

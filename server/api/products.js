0580207e47c3be816e24b79ded9e4c05e93fdac8
Lauryn Bassett <lauryn.bassett@gmail.com>
2020-04-22 10:23:07 -0500

    added product api routes, added account route in user api, added router and module.exports in each api file

---
+++ server/api/products.js
@@ -0,0 +1,79 @@
+const router = require('express').Router()
+import {Category, Product, Review, User} from '../db/models'
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll({
+      include: [{all: true}, {model: Review, include: [User]}]
+    })
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {name, description, price, imageUrl, quantity} = req.body
+    const newProduct = await Product.create({
+      name,
+      description,
+      price,
+      imageUrl,
+      quantity
+    })
+    const product = await Product.findByPk(newProduct.id, {
+      include: [
+        {all: true},
+        {model: Category},
+        {model: Review, include: [User]}
+      ]
+    })
+    res.status(201).json(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [
+        {all: true},
+        {model: Category},
+        {model: Review, include: [User]}
+      ]
+    })
+    product.update(req.body)
+    res.json(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/:id/review', async (req, res, next) => {
+  try {
+    const {rating, reviewText} = req.body
+    const userId = req.user.id
+    const productId = req.params.id
+    await Review.create({
+      userId,
+      productId,
+      rating,
+      reviewText
+    })
+    const reviewUser = await User.findByPk(req.user.id)
+    const reviewObj = {
+      user: reviewUser,
+      userId,
+      productId,
+      rating,
+      reviewText
+    }
+    res.json(reviewObj)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {Order, User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -11,6 +11,17 @@ router.get('/', async (req, res, next) => {
       attributes: ['id', 'email']
     })
     res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/account', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.user.id, {
+      include: [{all: true}, {model: Order, include: [{all: true}]}]
+    })
+    res.json(user)
   } catch (err) {
     next(err)
   }

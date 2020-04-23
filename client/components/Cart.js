a6aa3297b639ed0d3cc23529e608c3528da39b43
Jacob Wallin <jacobwallin@gmail.com>
2020-04-22 20:56:04 -0500

    created initial Cart component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,3 +1,34 @@
 // Cart Item component
 // mapState - state.cart.cart, state.cart.isFetching, state.user.id (loggedin)
 // mapDispatch - dispatch(addOrder) from accountsReducer
+
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchCart} from '../store/reducers/cartReducer'
+
+class Cart extends React.Component {
+  componentDidMount() {
+    this.props.getCart()
+  }
+
+  render() {
+    console.log(this.props.cart)
+    return (
+      <div className="cart">
+        <p>THIS IS A TEST</p>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    cart: state.cart.cart
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    getCart: () => dispatch(fetchCart())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
--- client/routes.js
+++ client/routes.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
-import {me, fetchAccount} from './store'
+import {Login, Signup, UserHome, Cart} from './components'
+import {me} from './store'
 
 /**
  * COMPONENT
@@ -21,6 +21,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/cart" component={Cart} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}

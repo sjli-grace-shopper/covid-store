384519d2a5899b3b8e0961ffe9cd94f63db7423f
Shreya Wadhwa <shreyawadhwa369@gmail.com>
2020-04-25 11:06:58 -0500

    product form formatting

--- client/components/ProductForm.js
+++ client/components/ProductForm.js
@@ -5,7 +5,18 @@ import {addProduct} from '../store/reducers/productReducer'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Link from '@material-ui/core/Link'
+import {
+  Paper,
+  Button,
+  RadioGroup,
+  FormLabel,
+  MenuItem,
+  FormGroup,
+  FormControl,
+  FormControlLabel
+} from '@material-ui/core'
 
 class ProductForm extends React.Component {
   constructor() {
@@ -37,82 +48,108 @@ class ProductForm extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Typography variant="h6" align="center" gutterBottom>
-          New Product Form:
-        </Typography>
-        <Grid container spacing={3}>
-          <form onSubmit={this.handleSubmit} className="productForm">
-            <div>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  className="productForm name"
-                  type="text"
-                  name="name"
-                  label="Product"
-                  align="center"
-                  fullWidth
-                  value={this.state.name}
-                  onChange={this.handleChange}
-                />
-              </Grid>
-            </div>
-            <div>
-              <Grid item xs={12} sm={20}>
-                <TextField
-                  className="productForm description"
-                  type="text"
-                  name="description"
-                  label="Description"
-                  fullWidth
-                  value={this.state.description}
-                  onChange={this.handleChange}
-                />
-              </Grid>
-            </div>
-            <label className="column" htmlFor="price">
-              price:
-            </label>
-            <input
-              className="productForm price"
-              type="double"
-              name="price"
-              value={this.state.price}
-              onChange={this.handleChange}
-            />
-            <label className="column" htmlFor="imageUrl">
-              Image Url:
-            </label>
-            <input
-              className="productForm imgUrl"
-              type="text"
-              name="imageUrl"
-              value={this.state.imageUrl}
-              onChange={this.handleChange}
-            />
-            <label className="column" htmlFor="quantity">
-              Quantity:
-            </label>
-            <input
-              className="productForm quantity"
-              type="integer"
-              name="quantity"
-              value={this.state.quantity}
-              onChange={this.handleChange}
-            />
-            <label className="column" htmlFor="category">
-              Category Number:
-            </label>
-            <input
-              className="productForm category"
-              type="integer"
-              name="category"
-              value={this.state.category}
-              onChange={this.handleChange}
-            />
-            <br />
-            <br />
-            <input type="submit" value="Submit" />
-          </form>
+        <Grid container alignItems="flex-start" spacing={2}>
+          <div style={{padding: 20, margin: 'auto', maxWidth: 600}}>
+            <CssBaseline />
+            <Paper style={{padding: 5}}>
+              <Typography
+                variant="h4"
+                align="center"
+                component="h1"
+                gutterBottom
+              >
+                New Product Form
+              </Typography>
+              <form onSubmit={this.handleSubmit} className="productForm">
+                <div>
+                  <Grid item xs={6}>
+                    <TextField
+                      className="productForm name"
+                      type="text"
+                      name="name"
+                      label="Product"
+                      align="center"
+                      fullWidth
+                      value={this.state.name}
+                      onChange={this.handleChange}
+                    />
+                  </Grid>
+                </div>
+                <div>
+                  <Grid item xs={12}>
+                    <TextField
+                      className="productForm description"
+                      type="text"
+                      name="description"
+                      label="Description"
+                      fullWidth
+                      value={this.state.description}
+                      onChange={this.handleChange}
+                    />
+                  </Grid>
+                </div>
+                <div>
+                  <Grid item xs={6}>
+                    <TextField
+                      className="productForm price"
+                      type="double"
+                      name="price"
+                      label="Price"
+                      fullWidth
+                      value={this.state.price}
+                      onChange={this.handleChange}
+                    />
+                  </Grid>
+                </div>
+                <div>
+                  <Grid item xs={12}>
+                    <TextField
+                      className="productForm imgUrl"
+                      type="text"
+                      name="imageUrl"
+                      label="URL"
+                      fullWidth
+                      value={this.state.imageUrl}
+                      onChange={this.handleChange}
+                    />
+                  </Grid>
+                </div>
+                <div>
+                  <Grid item xs={6}>
+                    <TextField
+                      className="productForm quantity"
+                      type="integer"
+                      name="quantity"
+                      label="Quantity"
+                      fullWidth
+                      value={this.state.quantity}
+                      onChange={this.handleChange}
+                    />
+                  </Grid>
+                </div>
+                <div>
+                  <Grid item xs={6}>
+                    <TextField
+                      className="productForm category"
+                      type="integer"
+                      name="category"
+                      label="Category"
+                      fullWidth
+                      value={this.state.category}
+                      onChange={this.handleChange}
+                    />
+                  </Grid>
+                </div>
+                <div>
+                  <Grid item style={{marginTop: 16}}>
+                    <Button variant="contained" type="submit" value="Submit">
+                      Submit
+                    </Button>
+                  </Grid>
+                </div>
+              </form>
+            </Paper>
+          </div>
         </Grid>
       </React.Fragment>
     )
